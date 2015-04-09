cbb0958a3da0d540db3bd0affb954d5c267d736f
Sacha Schlegel <sacha@schlegel.li>
2015-04-09 17:27:18 +0200

    Nun können auch Abos geändert werden.

--- my_ortoloco/views.py
+++ my_ortoloco/views.py
@@ -1167,7 +1167,8 @@ def my_future(request):
         'house_abos_future': house_abos_future,
         'small_abos_future': small_abos_future,
         'extras': extra_abos.itervalues(),
-        'abo_change_enabled': month is 12 or (month is 1 and day <= 6)
+        # 'abo_change_enabled': month is 12 or (month is 1 and day <= 6)
+        'abo_change_enabled': True
     })
     return render(request, 'future.html', renderdict)
 
