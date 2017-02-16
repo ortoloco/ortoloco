feecc8401414b2c9a80acded5a973000731f0741
motu55 <david.simmen@bluewin.ch>
2017-02-16 21:59:34 +0100

    removed my_ortoloco so tat it can come from the juntagrico thingy

--- my_ortoloco/personalisation/personal_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from my_ortoloco.personalisation import personal_views as my_ortoloco_personal
-
-urlpatterns = [
-    url('^my/politoloco$', my_ortoloco_personal.send_politoloco),
-    url(r'^photologue/', include('photologue.urls')),
-]
