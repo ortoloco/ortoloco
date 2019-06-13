a197df6e99b236773bd8fd1d8da6216f644cb3e5
David Simmen <david.simmen@bluewin.ch>
2019-06-13 23:27:20 +0200

    mailcopy logging

--- static_ortoloco/management/commands/mailcopy.py
+++ static_ortoloco/management/commands/mailcopy.py
@@ -1,9 +1,13 @@
 import imaplib
+import logging
+
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
+
+    logger = logging.getLogger(__name__)
+
     def add_arguments(self, parser):
         parser.add_argument('email', nargs=1)
         parser.add_argument('pst', nargs=1)
@@ -11,23 +15,23 @@ class Command(BaseCommand):
 
     # entry point used by manage.py
     def handle(self, *args, **options):
-        print('Start mailcopy')
+        self.logger.info('Start mailcopy')
         email = options['email'][0]
         pst = options['pst'][0]
         pcy = options['pcy'][0]
-        print('log on to servertown')
+        self.logger.info('log on to servertown')
         st_con = imaplib.IMAP4_SSL('www12.servertown.ch', 993)
         st_con.login(email, pst)
-        print('log on to cyon')
+        self.logger.info('log on to cyon')
         cy_con = imaplib.IMAP4_SSL('mail.cyon.ch', 993)
         cy_con.login(email, pcy)
         for box in st_con.list()[1]:
             box_str = str(box).split(' "." ')[1].replace('"', '').replace("'", '')
             st_con.select(box_str, readonly=False)  # open box which will have its contents copied
-            print('Fetching messages from \'%s\'...' % box_str)
+            self.logger.info('Fetching messages from \'%s\'...' % box_str)
             resp, items = st_con.search(None, 'ALL')  # get all messages in the box
             msg_nums = items[0].split()
-            print('%s messages to cyon' % len(msg_nums))
+            self.logger.info('%s messages to cyon' % len(msg_nums))
             if len(msg_nums) > 0:
                 cy_con.create(box_str)
             for msg_num in msg_nums:
@@ -36,9 +40,9 @@ class Command(BaseCommand):
                 flags = imaplib.ParseFlags(data[0][0])  # get flags
                 flags_str = [x.decode("utf-8") for x in flags]
                 flag_str = " ".join(flags_str)
-                print(flag_str)
+                self.logger.info(flag_str)
                 date = imaplib.Time2Internaldate(imaplib.Internaldate2tuple(data[0][0]))  # get date
                 copy_result = cy_con.append(box_str, flag_str, date, message)  # copy to archive
-                print(copy_result)
+                self.logger.info(copy_result)
         st_con.logout()
         cy_con.logout()
