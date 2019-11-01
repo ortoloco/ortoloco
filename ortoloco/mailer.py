195ebef746e7f58faccc284d23a9fe13bc460373
nixnuex <nixnuex@users.noreply.github.com>
2019-11-01 13:07:50 +0100

    Rollback

--- ortoloco/mailer.py
+++ ortoloco/mailer.py
@@ -1,12 +1,10 @@
 import copy
-import time
 
 from django.core import mail
 
 
 class Mailer:
     def send(msg):
-        starttime = time.time()
         tos = msg.to + msg.bcc
         plain_msg = copy.copy(msg)
         plain_msg.bcc = []
@@ -18,6 +16,3 @@ class Mailer:
             msgs.append(new_message)
         connection = mail.get_connection()
         connection.send_messages(msgs)
-
-        mails_duration = time.time() - starttime
-        print('Mailer sent {} Mails in {:.3f} seconds'.format(mails_count,mails_duration))
