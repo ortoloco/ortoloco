171facf937e0240f4413d01776059111c832e3a6
nixnuex <nixnuex@gmail.com>
2026-06-03 09:05:40 +0200

    More more jobs on front page

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -301,7 +301,7 @@ ENABLE_EXTERNAL_SIGNUP = True
 JOBS_FRONTPAGE = {
     'days': 14,
     'min': 3,
-    'max': 20,
+    'max': 30,
     'promoted_types': ["Aktionstag"],
     'promoted_count': 2
 }
